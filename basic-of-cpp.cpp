0acf8f5e75c16cf7d8f22783d76a0ef3582f4fd6
Raju kumar <raju631149@gmail.com>
2025-06-13 15:46:08 +0530

    working condition

---
+++ basic-of-cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x ; 
+    int y ;
+    cout<<"Find the area of rectangle is : "<<endl;
+    cout<<"Enter the value of lenght is : ";
+    cin>>x;
+    cout<<"Enter the value of breadth is : ";
+    cin>>y;
+    cout<<"Area of rectangle is length*breadth is:"<<x*y;
+    return 0;
+}
